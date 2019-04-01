8be58d07e6b434b7365cafde93e5919aecfc732e
Cheri Portman <cheriportman@hotmail.com>
2019-04-01 16:20:57 -0700

    initial test for ourMap passes

--- test/map.test.js
+++ test/map.test.js
@@ -1,9 +1,10 @@
 const {
-  map
+  ourMap
 } = require('../lib/map');
 
 describe('map function', () => {
   it('take array return array with no changes', () => {
-    expect(arr([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+    const mock = jest.fn(n => n);
+    expect(ourMap([1, 2, 3, 4], mock)).toEqual([1, 2, 3, 4]);
   });
 });
