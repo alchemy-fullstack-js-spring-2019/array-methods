c1e79cc258dcd3ab42468e4eb07664d630fcf928
Tommy Tran <tranttommy95@gmail.com>
2019-04-01 15:49:17 -0700

    map test passed for real

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,23 @@
 const map = require('../lib/index');
 
-describe('Map function`', () => {
+describe('Map function', () => {
+  const mock = jest.fn(n => n + 2);
+
   it('Applies function to every index in an array and returns a new array', () => {
     const expected = [2, 4, 6];
     const input = [0, 2, 4];
-    const mock = jest.fn(n => n + 2);
     const result = map(input, mock);
 
     expect(result).toEqual(expected);
     expect(mock).toHaveBeenCalledTimes(input.length);
+  });
+
+  it('map ignore holes in array', () => {
+    /* eslint-disable */
+    const expected = [2, 4,, 6];
+    const input = [0, 2,, 4];
+    const result = map(input, mock);
+  
+    expect(result).toEqual(expected);
   });
 });
