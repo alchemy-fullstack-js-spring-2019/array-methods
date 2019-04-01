779e5a08684ca70d6a78f9540f9033d22e2ae802
Tommy Tran <tranttommy95@gmail.com>
2019-04-01 16:01:14 -0700

    filter function passed

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
-const map = require('../lib/index');
+/* eslint-disable */
+const { map, filter } = require('../lib/index');
 
 describe('Map function', () => {
   const mock = jest.fn(n => n + 2);
@@ -13,11 +14,30 @@ describe('Map function', () => {
   });
 
   it('map ignore holes in array', () => {
-    /* eslint-disable */
     const expected = [2, 4,, 6];
     const input = [0, 2,, 4];
     const result = map(input, mock);
   
+    expect(result).toEqual(expected);
+    expect(result.length).toEqual(input.length);
+  });
+});
+
+describe('filter test', () => {
+  const mock = jest.fn(i => i > 5);
+  it('returns a new array filtered by conditional', () => {
+    const expected = [6, 8];
+    const input = [2, 4, 6, 8];
+    const result = filter(input, mock);
+
+    expect(result).toEqual(expected);
+  });
+  
+  it('filter filters out holes', () => {
+    const expected = [6, 8];
+    const input = [2, 4, 6,, 8];
+    const result = filter(input, mock);
+
     expect(result).toEqual(expected);
   });
 });
