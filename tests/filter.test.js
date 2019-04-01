ebd613675414c8d6e404efa83516adc1462492c3
ollinieminen <otn987@gmail.com>
2019-04-01 16:13:29 -0700

    one way to filter but maybe not the best way

---
+++ tests/filter.test.js
@@ -0,0 +1,8 @@
+const filter = require('../lib/filter');
+
+describe('array filter', () => {
+    it('takes array [1, 2, 4, 5] and isEven function and returns [2, 4]', () => {
+        const result = filter([1, 2, 4, 5], n => n % 2 === 0);
+        expect(result).toEqual([2, 4]);
+    });
+});
