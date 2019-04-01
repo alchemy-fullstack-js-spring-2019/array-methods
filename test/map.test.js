8b563befd205d033183b659cea0fcbd56fecdee3
Cheri Portman <cheriportman@hotmail.com>
2019-04-01 16:49:18 -0700

    we have made a map function that tests out!

--- test/map.test.js
+++ test/map.test.js
@@ -7,4 +7,12 @@ describe('map function', () => {
     const mock = jest.fn(n => n);
     expect(ourMap([1, 2, 3, 4], mock)).toEqual([1, 2, 3, 4]);
   });
+  it('deal with an array with holes', () => {
+    const mock = jest.fn(n => n);
+    expect(ourMap([1, 2,, 4], mock)).toEqual([1, 2,, 4]);
+  });
+  it('add map to our map', () => {
+    const mock = jest.fn(n => n * 2);
+    expect(ourMap([1, 2,, 4], mock)).toEqual([2, 4,, 8]);
+  });
 });
