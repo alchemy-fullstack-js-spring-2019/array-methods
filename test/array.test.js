eb8d34c924036845f3d626aef7c88c82164c87a7
emily baier <emilyrbaier@gmail.com>
2019-04-01 16:06:01 -0700

    TDD done for findIndex

--- test/array.test.js
+++ test/array.test.js
@@ -1,4 +1,4 @@
-const { map, filter } = require('../lib/array');
+const { map, filter, findIndex } = require('../lib/array');
 
 describe('map function', ()=> {
     it('takes an array and manipulates each item in the array, resulting in a new array', ()=> {
@@ -15,5 +15,18 @@ describe('filter function', ()=> {
         const result = filter([2, 4, 7], mock);
         expect(result).toEqual([2, 4]);
 
-    })
+    });
+});
+
+describe('findIndex function', ()=> {
+    it('this takes an array and number and returns the index of said number', ()=> {
+        const mock = jest.fn(n => n % 2 === 0);
+        const result = findIndex([5, 7, 9, 11, 8], mock);
+        expect(result).toEqual(4);
+    });
+    it('this takes an array and number and returns -1 if no index found', ()=> {
+        const mock = jest.fn(n => n % 2 === 0);
+        const result = findIndex([5, 7, 9, 11, 21], mock);
+        expect(result).toEqual(-1);
+    });
 });
