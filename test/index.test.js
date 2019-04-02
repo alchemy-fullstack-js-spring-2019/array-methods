13c3bb34a47d6c52bc72cf7e690d1aca71f5b9d2
Tommy Tran <tranttommy95@gmail.com>
2019-04-01 17:02:37 -0700

    forEach test passed

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
-/* eslint-disable */
-const { map, filter, findIndex, reduce } = require('../lib/index');
+/* eslint-disable no-sparse-arrays */
+const { map, filter, findIndex, reduce, every, forEach } = require('../lib/index');
 
 describe('Map function', () => {
   const mock = jest.fn(n => n + 2);
@@ -62,20 +62,60 @@ describe('findIndex function', () => {
 });
 
 describe('Reduce function', () => {
-  const mock = jest.fn((acc, n) => acc + n);
+  const mock = jest.fn((acc, n) => acc * n);
   it('return sum', () => {
-    const expected = 10;
-    const input = [1, 2, 3, 4];
+    const expected = 6;
+    const input = [1, 2, 3];
     const result = reduce(input, mock);
 
     expect(result).toEqual(expected);
   });
   
   it('return sum, with initial and hole in input', () => {
-    const expected = 20;
+    const expected = 240;
     const input = [1, 2, 3, , 4];
     const result = reduce(input, mock, 10);
 
     expect(result).toEqual(expected);
+  });
+});
+
+describe('every function', () => {
+  const mock = jest.fn(i => i > 5);
+  it('return true if all values meet conditional', () => {
+    const expected = true;
+    const input = [6, 7, 8, 9];
+    const result = every(input, mock);
+
+    expect(result).toEqual(expected);
+  });
+  it('return false if at least one value fails conditional', () => {
+    const expected = false;
+    const input = [6, 7, 8, 1];
+    const result = every(input, mock);
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('forEach function', () => {
+  it('performs callback on item in the array', () => {
+    const mock = jest.fn();
+    const expected = undefined;
+    const input = [1, 2, 3, 4, 5];
+    const result = forEach(input, mock);
+    
+    expect(result).toEqual(expected);
+    expect(mock).toHaveBeenCalledTimes(input.length);
+  });
+
+  it('performs callback on item in the array, ignoring holes', () => {
+    const mock = jest.fn();
+    const expected = undefined;
+    const input = [1, 2, 3, , 4, 5];
+    const result = forEach(input, mock);
+
+    expect(result).toEqual(expected);
+    expect(mock).toHaveBeenCalledTimes(5);
   });
 });
