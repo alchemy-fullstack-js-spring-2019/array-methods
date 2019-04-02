e03ed6b95daf9ce7aeed54c9c9034e2063cac094
ollinieminen <otn987@gmail.com>
2019-04-01 17:01:43 -0700

    reducer works!

---
+++ tests/reduce.test.js
@@ -0,0 +1,12 @@
+const reduce = require('../lib/reduce');
+
+describe('reduce', () => {
+    it('takes array [1, 2, 3] and callback sum and returns 6', () => {
+        const result = reduce([1, 2, 3], (n, a) => a += n);
+        expect(result).toBe(6);
+    });
+    it('takes array [1, 2, 3] callback sum, and starting value and returns 9', () => {
+        const result = reduce([1, 2, 3], (n, a) => a += n, 3);
+        expect(result).toBe(9);
+    });
+});
